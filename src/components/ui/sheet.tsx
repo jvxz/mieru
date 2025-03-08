a10c7d02bd74f5fc12edd617b1ac8745e87a4b6e
jamie 🌨️ <jamie@wisp.bio>
2025-03-08 18:49:14 -0500

    refactor: update sheet component

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -1,10 +1,8 @@
 'use client'
-
+import { interactiveStyles, overlayStyles, staticStyles } from '@/lib/styles'
 import { cn } from '@/lib/utils'
+import { Icon } from '@iconify/react'
 import * as SheetPrimitive from '@radix-ui/react-dialog'
-import { XIcon } from 'lucide-react'
-
-import * as React from 'react'
 
 function Sheet({ ...props }: React.ComponentProps<typeof SheetPrimitive.Root>) {
   return (
@@ -50,7 +48,7 @@ function SheetOverlay({
     <SheetPrimitive.Overlay
       data-slot="sheet-overlay"
       className={cn(
-        'data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 fixed inset-0 z-50 bg-black/80',
+        overlayStyles,
         className,
       )}
       {...props}
@@ -72,22 +70,58 @@ function SheetContent({
       <SheetPrimitive.Content
         data-slot="sheet-content"
         className={cn(
-          'bg-background fixed z-50 flex flex-col gap-4 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=open]:duration-500 data-[state=closed]:animate-out data-[state=closed]:duration-300',
+          staticStyles.base,
+          staticStyles.variant.default,
+          `
+            data-[state=open]:animate-in data-[state=open]:duration-150
+            fixed z-50 flex-col gap-4 rounded-none border-0 p-0 transition
+            ease-out
+          `,
           side === 'right'
-          && 'data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right inset-y-0 right-0 h-full w-3/4 border-l sm:max-w-sm',
+          && `
+            data-[state=closed]:slide-out-to-right
+            data-[state=open]:slide-in-from-right
+            inset-y-0 right-0 h-full w-3/4 border-l
+            sm:max-w-sm
+          `,
           side === 'left'
-          && 'data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left inset-y-0 left-0 h-full w-3/4 border-r sm:max-w-sm',
+          && `
+            data-[state=closed]:slide-out-to-left
+            data-[state=open]:slide-in-from-left
+            inset-y-0 left-0 h-full w-3/4 border-r
+            sm:max-w-sm
+          `,
           side === 'top'
-          && 'data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top inset-x-0 top-0 h-auto border-b',
+          && `
+            data-[state=closed]:slide-out-to-top
+            data-[state=open]:slide-in-from-top
+            inset-x-0 top-0 h-auto border-b
+          `,
           side === 'bottom'
-          && 'data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom inset-x-0 bottom-0 h-auto border-t',
+          && `
+            data-[state=closed]:slide-out-to-bottom
+            data-[state=open]:slide-in-from-bottom
+            inset-x-0 bottom-0 h-auto border-t
+          `,
           className,
         )}
         {...props}
       >
         {children}
-        <SheetPrimitive.Close className="ring-offset-background absolute top-4 right-4 rounded-xs opacity-70 transition-opacity focus:ring-ring focus:ring-2 focus:ring-offset-2 focus:outline-hidden data-[state=open]:bg-secondary hover:opacity-100 disabled:pointer-events-none">
-          <XIcon className="size-4" />
+        <SheetPrimitive.Close className={cn(
+          interactiveStyles.base,
+          interactiveStyles.variant.ghost,
+          interactiveStyles.size.icon,
+          `
+            ring-offset-background absolute top-4 right-4 inline-flex size-6
+            items-center justify-center opacity-70
+          `,
+        )}
+        >
+          <Icon
+            icon="ph:x"
+            className="size-4"
+          />
           <span className="sr-only">Close</span>
         </SheetPrimitive.Close>
       </SheetPrimitive.Content>
@@ -99,7 +133,7 @@ function SheetHeader({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
       data-slot="sheet-header"
-      className={cn('flex flex-col gap-1.5 p-4', className)}
+      className={cn('flex flex-col gap-1 p-4', className)}
       {...props}
     />
   )
@@ -122,7 +156,7 @@ function SheetTitle({
   return (
     <SheetPrimitive.Title
       data-slot="sheet-title"
-      className={cn('text-foreground font-semibold', className)}
+      className={cn('text-foreground text-md font-semibold', className)}
       {...props}
     />
   )
