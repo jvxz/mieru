7c373f4ab79a1d15aafb793d5a9dcb6b8e50b8fa
jamie 🌨️ <jamie@wisp.bio>
2025-03-08 18:30:08 -0500

    refactor: update seperator component

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,9 +1,6 @@
 'use client'
-
 import { cn } from '@/lib/utils'
 import * as SeparatorPrimitive from '@radix-ui/react-separator'
-
-import * as React from 'react'
 
 function Separator({
   className,
@@ -17,7 +14,12 @@ function Separator({
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        'bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px',
+        `
+          bg-border shrink-0
+          data-[orientation=horizontal]:h-px
+          data-[orientation=horizontal]:w-full
+          data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px
+        `,
         className,
       )}
       {...props}
