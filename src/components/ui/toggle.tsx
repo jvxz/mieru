9df2cd2ba1c8cbab18c951007821f43c9772daf6
jamie 🌨️ <jamie@wisp.bio>
2025-03-08 19:56:14 -0500

    refactor: update toggle component

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -1,25 +1,43 @@
 'use client'
-
 import type { VariantProps } from 'class-variance-authority'
+import { interactiveStyles } from '@/lib/styles'
 import { cn } from '@/lib/utils'
 import * as TogglePrimitive from '@radix-ui/react-toggle'
 import { cva } from 'class-variance-authority'
 
-import * as React from 'react'
-
 const toggleVariants = cva(
-  'hover:bg-muted hover:text-muted-foreground data-[state=on]:bg-accent data-[state=on]:text-accent-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 aria-invalid:border-destructive dark:aria-invalid:ring-destructive/40 inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium whitespace-nowrap transition-[color,box-shadow] outline-none disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*=\'size-\'])]:size-4',
+  cn(interactiveStyles.base, `
+    data-[state=on]:bg-accent data-[state=on]:text-accent-foreground
+    p-0
+  `),
+  // `
+  //   hover:bg-muted hover:text-muted-foreground
+  //   data-[state=on]:bg-accent data-[state=on]:text-accent-foreground
+  //   focus-visible:border-ring focus-visible:ring-ring/50
+  //   focus-visible:ring-[3px]
+  //   aria-invalid:ring-destructive/20 aria-invalid:border-destructive
+  //   dark:aria-invalid:ring-destructive/40
+  //   inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium
+  //   whitespace-nowrap transition-[color,box-shadow] outline-none
+  //   disabled:pointer-events-none disabled:opacity-50
+  //   [&_svg]:pointer-events-none [&_svg]:shrink-0
+  //   [&_svg:not([class*='size-'])]:size-4
+  // `,
   {
     variants: {
       variant: {
-        default: 'bg-transparent',
-        outline:
-          'border-input border bg-transparent shadow-xs hover:bg-accent hover:text-accent-foreground',
+        default: [interactiveStyles.variant.ghost, `
+          m-0 border border-transparent
+        `],
+        outline: [interactiveStyles.variant.outline, `
+          data-[state=on]:border-primary/30
+          m-0
+        `],
       },
       size: {
-        default: 'h-9 min-w-9 px-2',
-        sm: 'h-8 min-w-8 px-1.5',
-        lg: 'h-10 min-w-10 px-2.5',
+        default: [interactiveStyles.size.default, 'text-sm'],
+        sm: [interactiveStyles.size.sm, 'text-sm'],
+        lg: [interactiveStyles.size.lg, 'text-sm'],
       },
     },
     defaultVariants: {
