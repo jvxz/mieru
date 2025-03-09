1921c9cb929a698ef29eb14180e4940d3471b7a4
jamie 🌨️ <jamie@wisp.bio>
2025-03-08 19:43:21 -0500

    refactor: update textarea component

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,13 +1,19 @@
+import { interactiveStyles } from '@/lib/styles'
 import { cn } from '@/lib/utils'
-
-import * as React from 'react'
 
 function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
   return (
     <textarea
       data-slot="textarea"
       className={cn(
-        'border-input flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 aria-invalid:border-destructive dark:aria-invalid:ring-destructive/40 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+        interactiveStyles.base,
+        interactiveStyles.variant.default,
+        `
+          placeholder:text-muted-foreground
+          field-sizing-content min-h-16 w-full cursor-text p-2
+          transition-[color,box-shadow,opacity]
+          md:text-sm
+        `,
         className,
       )}
       {...props}
