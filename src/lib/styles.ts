5a601b7e2553394a11bb56ae3d801da459336d21
jamie 🌨️ <jamie@wisp.bio>
2025-03-08 20:26:41 -0500

    refactor: remove shadows from most components

--- src/lib/styles.ts
+++ src/lib/styles.ts
@@ -1,14 +1,14 @@
 const interactiveStyles = {
   base: 'focus-visible:border-ring focus-visible:ring-ring/40 focus-visible:ring-[3px] active:ring-ring/20 aria-invalid:ring-destructive/20 aria-invalid:border-destructive dark:aria-invalid:ring-destructive/40 shrink-0 cursor-pointer gap-2 rounded text-base whitespace-nowrap transition-all outline-none disabled:pointer-events-none hover:disabled:cursor-not-allowed disabled:opacity-50 underline-offset-4 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*=\'size-\'])]:size-4 duration-100',
   variant: {
-    default: 'bg-secondary/50 border-border text-secondary-foreground border shadow hover:border-primary/20 active:border-primary/35',
-    destructive: 'bg-destructive/90 text-destructive-foreground shadow hover:bg-destructive active:bg-destructive/90',
+    default: 'bg-secondary/50 border-border text-secondary-foreground border  hover:border-primary/20 active:border-primary/35',
+    destructive: 'bg-destructive/90 text-destructive-foreground  hover:bg-destructive active:bg-destructive/90',
     outline: 'border-border/60 border bg-transparent hover:bg-secondary/50 hover:border-primary/20 active:border-primary/35',
     ghost: 'hover:bg-secondary active:bg-secondary/80',
     link: 'text-primary underline-offset-4 hover:underline',
   },
   size: {
-    default: 'h-8 px-3 py-1.5 text-base',
+    default: 'h-8 px-3 py-1.5 text-sm',
     sm: 'h-7 px-2.5 py-1 text-sm',
     lg: 'h-10 px-5 text-base text-lg',
     icon: 'aspect-square size-8',
@@ -16,7 +16,7 @@ const interactiveStyles = {
 }
 
 const staticStyles = {
-  base: 'rounded p-5 shadow duration-100',
+  base: 'rounded p-5  duration-100',
   variant: {
     default: 'bg-card border text-card-foreground',
     destructive: 'bg-card border-destructive text-destructive border [&>svg]:text-current',
@@ -46,7 +46,7 @@ const popoverStyles = {
             data-[side=left]:slide-in-from-right-2
             data-[side=right]:slide-in-from-left-2
             data-[side=top]:slide-in-from-bottom-2
-            z-50 min-w-[8rem] overflow-hidden p-1 shadow duration-100
+            z-50 min-w-[8rem] overflow-hidden p-1  duration-100
           `],
 }
 
