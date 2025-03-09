69de231c918470dedd6bb7c58bf6a77f869a4b98
jamie 🌨️ <jamie@wisp.bio>
2025-03-08 19:24:40 -0500

    refactor: update switch component

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,9 +1,7 @@
 'use client'
-
+import { interactiveStyles, staticStyles } from '@/lib/styles'
 import { cn } from '@/lib/utils'
 import * as SwitchPrimitive from '@radix-ui/react-switch'
-
-import * as React from 'react'
 
 function Switch({
   className,
@@ -13,14 +11,29 @@ function Switch({
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
-        'peer inline-flex h-5 w-9 shrink-0 items-center rounded-full border-2 border-transparent shadow-xs transition-all outline-none data-[state=checked]:bg-primary data-[state=unchecked]:bg-input focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50',
+        staticStyles.base,
+        staticStyles.variant.default,
+        `
+          peer bg-accent border-accent inline-flex h-5 w-9 items-center
+          rounded-full border-2 p-0
+          data-[state=checked]:bg-primary data-[state=checked]:border-primary
+        `,
         className,
       )}
       {...props}
     >
       <SwitchPrimitive.Thumb
         data-slot="switch-thumb"
-        className={cn('bg-background pointer-events-none block size-4 rounded-full shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0')}
+        className={cn(
+          interactiveStyles.base,
+          interactiveStyles.variant.default,
+          `
+            bg-background pointer-events-none block size-4 rounded-full border-0
+            ring-0 transition-transform
+            data-[state=checked]:translate-x-4
+            data-[state=unchecked]:translate-x-0
+          `,
+        )}
       />
     </SwitchPrimitive.Root>
   )
