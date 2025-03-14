54f849dd611dcae9ee87290edba064e2998e8a76
jamie 🌨️ <jamie@wisp.bio>
2025-03-14 14:26:53 -0400

    refactor: update resizable component

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,20 +1,18 @@
 'use client'
+import type { ComponentProps } from 'react'
 import { cn } from '@/lib/utils'
-import { Icon } from '@iconify/react/dist/iconify.js'
+import { Icon } from '@iconify/react'
 import * as ResizablePrimitive from 'react-resizable-panels'
 
 function ResizablePanelGroup({
   className,
   ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) {
+}: ComponentProps<typeof ResizablePrimitive.PanelGroup>) {
   return (
     <ResizablePrimitive.PanelGroup
       data-slot="resizable-panel-group"
       className={cn(
-        `
-          flex h-full w-full
-          data-[panel-group-direction=vertical]:flex-col
-        `,
+        'flex h-full w-full data-[panel-group-direction=vertical]:flex-col',
         className,
       )}
       {...props}
@@ -22,7 +20,7 @@ function ResizablePanelGroup({
   )
 }
 
-function ResizablePanel({ ...props }: React.ComponentProps<typeof ResizablePrimitive.Panel>) {
+function ResizablePanel({ ...props }: ComponentProps<typeof ResizablePrimitive.Panel>) {
   return (
     <ResizablePrimitive.Panel
       data-slot="resizable-panel"
@@ -35,38 +33,20 @@ function ResizableHandle({
   withHandle,
   className,
   ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
+}: ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
   withHandle?: boolean
 }) {
   return (
     <ResizablePrimitive.PanelResizeHandle
       data-slot="resizable-handle"
       className={cn(
-        `
-          bg-border relative flex w-px items-center justify-center
-          focus-visible:ring-ring focus-visible:ring-1
-          focus-visible:ring-offset-1 focus-visible:outline-hidden
-          after:absolute after:inset-y-0 after:left-1/2 after:w-1
-          after:-translate-x-1/2
-          data-[panel-group-direction=vertical]:h-px
-          data-[panel-group-direction=vertical]:w-full
-          data-[panel-group-direction=vertical]:after:left-0
-          data-[panel-group-direction=vertical]:after:h-1
-          data-[panel-group-direction=vertical]:after:w-full
-          data-[panel-group-direction=vertical]:after:translate-x-0
-          data-[panel-group-direction=vertical]:after:-translate-y-1/2
-          [&[data-panel-group-direction=vertical]>div]:rotate-90
-        `,
+        'bg-border relative flex w-px items-center justify-center focus-visible:ring-ring focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2 [&[data-panel-group-direction=vertical]>div]:rotate-90',
         className,
       )}
       {...props}
     >
       {withHandle && (
-        <div className={`
-          bg-border z-10 flex h-4 w-3 items-center justify-center rounded-xs
-          border
-        `}
-        >
+        <div className="bg-border z-10 flex h-4 w-3 items-center justify-center rounded-xs border">
           <Icon
             icon="ph:dots-six-vertical-bold"
             className="size-3"
