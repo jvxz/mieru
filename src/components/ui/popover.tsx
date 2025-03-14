b7d69c89aa10e4872f2ed4fc7969914144f51746
jamie 🌨️ <jamie@wisp.bio>
2025-03-14 14:26:04 -0400

    refactor: update popover component

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -1,10 +1,10 @@
 'use client'
+import type { ComponentProps } from 'react'
 import { popoverStyles } from '@/lib/styles'
 import { cn } from '@/lib/utils'
 import * as PopoverPrimitive from '@radix-ui/react-popover'
-import * as React from 'react'
 
-function Popover({ ...props }: React.ComponentProps<typeof PopoverPrimitive.Root>) {
+function Popover({ ...props }: ComponentProps<typeof PopoverPrimitive.Root>) {
   return (
     <PopoverPrimitive.Root
       data-slot="popover"
@@ -13,7 +13,7 @@ function Popover({ ...props }: React.ComponentProps<typeof PopoverPrimitive.Root
   )
 }
 
-function PopoverTrigger({ ...props }: React.ComponentProps<typeof PopoverPrimitive.Trigger>) {
+function PopoverTrigger({ ...props }: ComponentProps<typeof PopoverPrimitive.Trigger>) {
   return (
     <PopoverPrimitive.Trigger
       data-slot="popover-trigger"
@@ -27,7 +27,7 @@ function PopoverContent({
   align = 'center',
   sideOffset = 4,
   ...props
-}: React.ComponentProps<typeof PopoverPrimitive.Content>) {
+}: ComponentProps<typeof PopoverPrimitive.Content>) {
   return (
     <PopoverPrimitive.Portal>
       <PopoverPrimitive.Content
@@ -36,10 +36,7 @@ function PopoverContent({
         sideOffset={sideOffset}
         className={cn(
           popoverStyles.content,
-          `
-            data-[state=open]:slide-in-from-top-2
-            p-4
-          `,
+          'p-2 text-sm',
           className,
         )}
         {...props}
@@ -48,7 +45,7 @@ function PopoverContent({
   )
 }
 
-function PopoverAnchor({ ...props }: React.ComponentProps<typeof PopoverPrimitive.Anchor>) {
+function PopoverAnchor({ ...props }: ComponentProps<typeof PopoverPrimitive.Anchor>) {
   return (
     <PopoverPrimitive.Anchor
       data-slot="popover-anchor"
