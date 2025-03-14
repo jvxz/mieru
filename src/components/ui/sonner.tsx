ec7c0e7466d8e9d3950eee1fde5a47642a900acb
jamie 🌨️ <jamie@wisp.bio>
2025-03-14 14:28:00 -0400

    refactor: update sonner component

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,19 +1,14 @@
 'use client'
 import type { ToasterProps } from 'sonner'
-import { useTheme } from 'next-themes'
 import { Toaster as Sonner } from 'sonner'
 
 function Toaster({ ...props }: ToasterProps) {
-  const { theme = 'system' } = useTheme()
-
   return (
     <Sonner
-      theme={theme as ToasterProps['theme']}
-      className="toaster group"
       toastOptions={{
         classNames: {
           toast:
-            'group toast !bg-card !text-card-foreground !border-border !shadow',
+            'group toast !bg-card !text-card-foreground !border-border !shadow-none',
           description: '!text-muted-foreground',
           actionButton:
             '!bg-primary !text-primary-foreground font-medium',
