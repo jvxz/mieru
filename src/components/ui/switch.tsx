e164acf04626afa216c0079732dab80a2fcbf659
jamie 🌨️ <jamie@wisp.bio>
2025-03-14 14:28:44 -0400

    refactor: update switch component

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,4 +1,5 @@
 'use client'
+import type { ComponentProps } from 'react'
 import { interactiveStyles, staticStyles } from '@/lib/styles'
 import { cn } from '@/lib/utils'
 import * as SwitchPrimitive from '@radix-ui/react-switch'
@@ -6,18 +7,14 @@ import * as SwitchPrimitive from '@radix-ui/react-switch'
 function Switch({
   className,
   ...props
-}: React.ComponentProps<typeof SwitchPrimitive.Root>) {
+}: ComponentProps<typeof SwitchPrimitive.Root>) {
   return (
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
         staticStyles.base,
         staticStyles.variant.default,
-        `
-          peer bg-accent border-accent inline-flex h-5 w-9 items-center
-          rounded-full border-2 p-0
-          data-[state=checked]:bg-primary data-[state=checked]:border-primary
-        `,
+        'bg-muted border-muted inline-flex h-5 w-9 items-center rounded-full border-2 p-0 data-[state=checked]:bg-primary data-[state=checked]:border-primary',
         className,
       )}
       {...props}
@@ -27,12 +24,7 @@ function Switch({
         className={cn(
           interactiveStyles.base,
           interactiveStyles.variant.default,
-          `
-            bg-background pointer-events-none block size-4 rounded-full border-0
-            ring-0 transition-transform
-            data-[state=checked]:translate-x-4
-            data-[state=unchecked]:translate-x-0
-          `,
+          'bg-background pointer-events-none block size-4 rounded-full border-0 ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0',
         )}
       />
     </SwitchPrimitive.Root>
