d9cc506f06a7a8e3721ce7a5ec2ad9a32ad5370c
jamie 🌨️ <jamie@wisp.bio>
2025-03-14 14:23:48 -0400

    refactor: update input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,27 +1,13 @@
+import type { ComponentProps } from 'react'
 import { interactiveStyles } from '@/lib/styles'
 import { cn } from '@/lib/utils'
-import * as React from 'react'
 
-function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
+function Input({ className, type, ...props }: ComponentProps<'input'>) {
   return (
     <input
       type={type}
       data-slot="input"
-      className={cn(
-        interactiveStyles.base,
-        interactiveStyles.variant.default,
-        interactiveStyles.size.default,
-        `
-          file:text-foreground file:inline-flex file:h-7 file:border-0
-          file:bg-transparent file:text-sm file:font-medium
-          placeholder:text-muted-foreground
-          selection:bg-primary selection:text-primary-foreground
-          focus-visible:border-ring/40 focus-visible:ring-0
-          flex w-full min-w-0 cursor-text px-3 py-1 transition-all
-          md:text-sm
-        `,
-        className,
-      )}
+      className={cn(interactiveStyles.base, interactiveStyles.variant.default, interactiveStyles.size.default, 'file:text-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex w-full min-w-0 cursor-text px-3 py-1 transition-all focus-visible:ring-0 md:text-sm', className)}
       {...props}
     />
   )
