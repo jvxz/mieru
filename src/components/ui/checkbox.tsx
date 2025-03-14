45b255dd79c2fbabe5000031322c393354eceb4d
jamie 🌨️ <jamie@wisp.bio>
2025-03-14 13:43:59 -0400

    refactor: update checkbox component

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,36 +1,23 @@
 'use client'
+import type { ComponentProps } from 'react'
 import { interactiveStyles } from '@/lib/styles'
 import { cn } from '@/lib/utils'
 import { Icon } from '@iconify/react'
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
-import * as React from 'react'
 
 function Checkbox({
   className,
   ...props
-}: React.ComponentProps<typeof CheckboxPrimitive.Root>) {
+}: ComponentProps<typeof CheckboxPrimitive.Root>) {
   return (
     <CheckboxPrimitive.Root
       data-slot="checkbox"
-      className={cn(
-        interactiveStyles.base,
-        interactiveStyles.variant.default,
-        `
-          active:data-[state=checked]:bg-primary/90
-          data-[state=checked]:bg-primary
-          data-[state=checked]:text-primary-foreground
-          data-[state=checked]:border-primary
-          size-4 cursor-default rounded-sm transition-shadow
-        `,
-        className,
-      )}
+      className={cn(interactiveStyles.base, interactiveStyles.variant.default, 'active:data-[state=checked]:bg-primary/90 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground data-[state=checked]:border-primary size-4 cursor-default rounded-sm transition-shadow', className)}
       {...props}
     >
       <CheckboxPrimitive.Indicator
         data-slot="checkbox-indicator"
-        className={`
-          flex items-center justify-center text-current transition-none
-        `}
+        className="flex items-center justify-center text-current transition-none"
       >
         <Icon
           icon="ph:check-bold"
