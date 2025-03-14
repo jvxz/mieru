ca794241ced15e3f1af4fc24aba1d273d50cd176
jamie 🌨️ <jamie@wisp.bio>
2025-03-14 14:29:17 -0400

    refactor: update toggle component

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -1,38 +1,18 @@
 'use client'
 import type { VariantProps } from 'class-variance-authority'
+import type { ComponentProps } from 'react'
 import { interactiveStyles } from '@/lib/styles'
 import { cn } from '@/lib/utils'
 import * as TogglePrimitive from '@radix-ui/react-toggle'
 import { cva } from 'class-variance-authority'
 
 const toggleVariants = cva(
-  cn(interactiveStyles.base, `
-    data-[state=on]:bg-accent data-[state=on]:text-accent-foreground
-    p-0
-  `),
-  // `
-  //   hover:bg-muted hover:text-muted-foreground
-  //   data-[state=on]:bg-accent data-[state=on]:text-accent-foreground
-  //   focus-visible:border-ring focus-visible:ring-ring/50
-  //   focus-visible:ring-[3px]
-  //   aria-invalid:ring-destructive/20 aria-invalid:border-destructive
-  //   dark:aria-invalid:ring-destructive/40
-  //   inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium
-  //   whitespace-nowrap transition-[color,box-shadow] outline-none
-  //   disabled:pointer-events-none disabled:opacity-50
-  //   [&_svg]:pointer-events-none [&_svg]:shrink-0
-  //   [&_svg:not([class*='size-'])]:size-4
-  // `,
+  cn(interactiveStyles.base, `${interactiveStyles.base} data-[state=on]:bg-accent data-[state=on]:text-accent-foreground p-0`),
   {
     variants: {
       variant: {
-        default: [interactiveStyles.variant.ghost, `
-          m-0 border border-transparent
-        `],
-        outline: [interactiveStyles.variant.outline, `
-          data-[state=on]:border-primary/30
-          m-0
-        `],
+        default: [interactiveStyles.variant.ghost, 'data-[state=on]:bg-muted m-0 border border-transparent'],
+        outline: [interactiveStyles.variant.outline, 'data-[state=on]:bg-muted data-[state=on]:border-primary/30 m-0'],
       },
       size: {
         default: [interactiveStyles.size.default, 'text-sm'],
@@ -52,7 +32,7 @@ function Toggle({
   variant,
   size,
   ...props
-}: React.ComponentProps<typeof TogglePrimitive.Root> &
+}: ComponentProps<typeof TogglePrimitive.Root> &
   VariantProps<typeof toggleVariants>) {
   return (
     <TogglePrimitive.Root
