1dd31065ab81688d7410939926c43d2ddd07df64
jamie 🌨️ <jamie@wisp.bio>
2025-03-14 14:23:39 -0400

    refactor: update input otp component

--- src/components/ui/input-otp.tsx
+++ src/components/ui/input-otp.tsx
@@ -1,34 +1,29 @@
 'use client'
+import type { ComponentProps } from 'react'
 import { interactiveStyles } from '@/lib/styles'
 import { cn } from '@/lib/utils'
-import { Icon } from '@iconify/react/dist/iconify.js'
+import { Icon } from '@iconify/react'
 import { OTPInput, OTPInputContext } from 'input-otp'
-import * as React from 'react'
+import { useContext } from 'react'
 
 function InputOTP({
   className,
   containerClassName,
   ...props
-}: React.ComponentProps<typeof OTPInput> & {
+}: ComponentProps<typeof OTPInput> & {
   containerClassName?: string
 }) {
   return (
     <OTPInput
       data-slot="input-otp"
-      containerClassName={cn(
-        `
-          flex items-center gap-2
-          has-disabled:opacity-50
-        `,
-        containerClassName,
-      )}
+      containerClassName={cn('flex items-center gap-2 has-disabled:opacity-50', containerClassName)}
       className={cn('disabled:cursor-not-allowed', className)}
       {...props}
     />
   )
 }
 
-function InputOTPGroup({ className, ...props }: React.ComponentProps<'div'>) {
+function InputOTPGroup({ className, ...props }: ComponentProps<'div'>) {
   return (
     <div
       data-slot="input-otp-group"
@@ -42,10 +37,10 @@ function InputOTPSlot({
   index,
   className,
   ...props
-}: React.ComponentProps<'div'> & {
+}: ComponentProps<'div'> & {
   index: number
 }) {
-  const inputOTPContext = React.useContext(OTPInputContext)
+  const inputOTPContext = useContext(OTPInputContext)
   const { char, hasFakeCaret, isActive } = inputOTPContext?.slots[index] ?? {
   }
 
@@ -56,40 +51,22 @@ function InputOTPSlot({
       className={cn(
         interactiveStyles.base,
         interactiveStyles.variant.default,
-        `
-          data-[active=true]:border-ring data-[active=true]:ring-ring/50
-          data-[active=true]:aria-invalid:ring-destructive/20
-          data-[active=true]:aria-invalid:border-destructive
-          data-[active=true]:z-10 data-[active=true]:ring-[3px]
-          dark:data-[active=true]:aria-invalid:ring-destructive/40
-          aria-invalid:border-destructive
-          relative flex size-9 items-center justify-center rounded-none
-          border-x-0 border-y border-r text-sm shadow-xs transition-all
-          outline-none
-          first:rounded-l-md first:border-l
-          last:rounded-r-md
-        `,
+        'data-[active=true]:border-ring data-[active=true]:ring-ring/50 data-[active=true]:aria-invalid:ring-destructive/20 data-[active=true]:aria-invalid:border-destructive data-[active=true]:z-10 data-[active=true]:ring-[3px] dark:data-[active=true]:aria-invalid:ring-destructive/40 aria-invalid:border-destructive relative flex size-9 items-center justify-center rounded-none border-x-0 border-y border-r text-sm shadow-sm transition outline-none first:rounded-l-md first:border-l last:rounded-r-md',
         className,
       )}
       {...props}
     >
       {char}
       {hasFakeCaret && (
-        <div className={`
-          pointer-events-none absolute inset-0 flex items-center justify-center
-        `}
-        >
-          <div className={`
-            animate-caret-blink bg-foreground h-4 w-px duration-1000
-          `}
-          />
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
+          <div className="animate-caret-blink bg-ring h-4 w-px duration-1000" />
         </div>
       )}
     </div>
   )
 }
 
-function InputOTPSeparator({ ...props }: React.ComponentProps<'div'>) {
+function InputOTPSeparator({ ...props }: ComponentProps<'div'>) {
   return (
     <div
       data-slot="input-otp-separator"
