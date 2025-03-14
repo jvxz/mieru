e3b98edaae361c6818bc6713313e0da4e160c2aa
jamie 🌨️ <jamie@wisp.bio>
2025-03-14 14:27:16 -0400

    refactor: update separator component

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,4 +1,5 @@
 'use client'
+import type { ComponentProps } from 'react'
 import { cn } from '@/lib/utils'
 import * as SeparatorPrimitive from '@radix-ui/react-separator'
 
@@ -7,19 +8,14 @@ function Separator({
   orientation = 'horizontal',
   decorative = true,
   ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+}: ComponentProps<typeof SeparatorPrimitive.Root>) {
   return (
     <SeparatorPrimitive.Root
       data-slot="separator-root"
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        `
-          bg-border shrink-0
-          data-[orientation=horizontal]:h-px
-          data-[orientation=horizontal]:w-full
-          data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px
-        `,
+        'bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px',
         className,
       )}
       {...props}
