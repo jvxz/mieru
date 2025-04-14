e018a2b830d84a7f579fad6d64d6ea29c5b447ae
jamie 🌨️ <jamie@wisp.bio>
2025-04-14 13:46:31 -0400

    refactor: update header

--- src/app/(index)/layout.tsx
+++ src/app/(index)/layout.tsx
@@ -1,5 +1,6 @@
 import { ThemeToggle } from '@/components/theme-toggle'
 import { Card } from '@/components/ui/card'
+import { Icon } from '@iconify/react/dist/iconify.js'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -13,9 +14,21 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 function Header() {
   return (
     <header>
-      <Card className="bg-card my-4 flex h-12 flex-row items-center justify-between rounded border p-0 px-4">
-        <h1>mieru</h1>
-
+      <Card className="bg-card my-4 flex h-16 flex-row items-center justify-between rounded border p-0 px-6">
+        <div className="flex items-center gap-1 text-xl">
+          <Icon
+            icon="uil:rainbow"
+            strokeWidth={0.5}
+          />
+          <h1 className="font-medium">mieru</h1>
+        </div>
+        {/* <Button
+          variant="outline"
+          className="w-1/3"
+        >
+          Search
+          <Icon icon="mingcute:search-line" />
+        </Button> */}
         <ThemeToggle />
       </Card>
     </header>
