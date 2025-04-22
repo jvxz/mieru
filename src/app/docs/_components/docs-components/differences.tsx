2bfd5f60b9098ab126acf5d518a09a049f93db74
jamie 🌨️ <jamie@wisp.bio>
2025-04-22 17:27:21 -0400

    refactor: finish differences components

--- src/app/docs/_components/docs-components/differences.tsx
+++ src/app/docs/_components/docs-components/differences.tsx
@@ -1,12 +1,14 @@
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { Button } from '@/components/ui/button'
+import { Button, buttonVariants } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/checkbox'
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card'
 import { Label } from '@/components/ui/label'
 import { Switch } from '@/components/ui/switch'
+import { cn } from '@/lib/utils'
 
 function StaticExamples() {
   return (
@@ -23,14 +25,14 @@ function StaticExamples() {
 function InteractiveExamples() {
   return (
     <div className="flex items-center justify-center gap-6">
-      <Button>Interactive</Button>
+      <Button>Button</Button>
       <div className="flex items-center gap-2">
         <Checkbox id="checkbox" />
-        <Label htmlFor="checkbox">Interactive</Label>
+        <Label htmlFor="checkbox">Checkbox</Label>
       </div>
       <div className="flex items-center gap-2">
         <Switch id="switch" />
-        <Label htmlFor="switch">Interactive</Label>
+        <Label htmlFor="switch">Switch</Label>
       </div>
     </div>
   )
@@ -41,42 +43,39 @@ function PopoverExamples() {
     <div className="flex items-center justify-center gap-6">
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button>Dropdown</Button>
+          <Button>Settings</Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
           <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>Subscription</DropdownMenuItem>
+          <DropdownMenuItem>Appearance</DropdownMenuItem>
+          <DropdownMenuItem>Notifications</DropdownMenuItem>
+          <DropdownMenuItem variant="destructive">Logout</DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
       <HoverCard>
         <HoverCardTrigger asChild>
-          <Button variant="link">@nextjs</Button>
+          <Button variant="link">@raycastapp</Button>
         </HoverCardTrigger>
         <HoverCardContent
           side="top"
-          className="w-80"
+          className="w-66"
         >
-          <div className="flex justify-between space-x-4">
+          <div className="flex justify-between">
             <Avatar>
-              <AvatarImage src="https://github.com/vercel.png" />
-              <AvatarFallback>VC</AvatarFallback>
+              <AvatarImage src="https://github.com/raycast.png" />
+              <AvatarFallback>RC</AvatarFallback>
             </Avatar>
-            <div className="space-y-4">
-              <h4 className="text-sm font-semibold">@nextjs</h4>
+            <div className="space-y-1">
+              <h4 className="text-sm font-semibold">@raycastapp</h4>
               <p className="text-sm">
-                The React Framework – created and maintained by @vercel.
+                Your shortcut to everything.
               </p>
               <div className="flex items-center pt-2">
                 <div
-                  className="iconify mr-2 h-4 w-4 opacity-70"
-                  data-icon="ph:calendar-blank"
+                  className="iconify icon-[ph--calendar-blank] text-muted-foreground mr-2 h-4 w-4 opacity-70"
                 />
                 <span className="text-muted-foreground text-xs">
-                  Joined December 2021
+                  November 2019
                 </span>
               </div>
             </div>
@@ -91,20 +90,30 @@ function PopoverExamples() {
 function OverlayExamples() {
   return (
     <div className="flex justify-center">
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button>Dialog</Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your account
-              and remove your data from our servers.
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button>Archive</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Archive project</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action is irreversible. Your project will be archived
+              and all associated resources will be released from your workspace.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction className={
+              cn(buttonVariants({
+                variant: 'destructive',
+              }),
+                'border-0'
+              )
+            }>Archive</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   )
 }
